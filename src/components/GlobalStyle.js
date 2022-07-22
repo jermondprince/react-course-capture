5bb2bfc858742068acf7b19ab592843695d3a405
Jermond Prince <jermondprince@gmail.com>
2022-07-22 15:00:58 -0500

    6. Styling Service Section

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -9,6 +9,7 @@ const GlobalStyle = createGlobalStyle`
 
     body {
         background: #1b1b1b;
+        font-family: "Inter", sans-serif;
     }
 
     button{
@@ -20,6 +21,8 @@ const GlobalStyle = createGlobalStyle`
         border: 3px solid #23d997;
         color: white;
         transition: all 0.5s ease;
+        font-family: "Inter", sans-serif;
+
         &:hover{
             background-color: #23d997;
             color: white;
